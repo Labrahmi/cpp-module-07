d72d9e9724e17dc9557b3e4bc8cff58fc28a7a28
youssef labrahmi <ylabrahm@c1r8p3.1337.ma>
2023-10-09 09:59:29 +0100

    up

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ylabrahm <ylabrahm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 07:47:24 by ylabrahm          #+#    #+#             */
-/*   Updated: 2023/10/09 08:32:07 by ylabrahm         ###   ########.fr       */
+/*   Updated: 2023/10/09 09:58:25 by ylabrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,29 @@
 
 void execute_array()
 {
-    Array<std::string> myArray(10);
-    for (unsigned int i = 0; i < myArray.size(); i++)
+    try
     {
-        std::string newStr;
-        newStr = "T[] = ";
-        newStr += std::to_string(i + 1);
-        myArray[i] = newStr;
+        Array<std::string> myArray(10);
+        for (unsigned int i = 0; i < myArray.size(); i++)
+        {
+            std::string newStr;
+            newStr = "T[] = ";
+            newStr += std::to_string(i + 1);
+            myArray[i] = newStr;
+        }
+        for (unsigned int i = 0; i < myArray.size(); i++)
+            std::cout << myArray[i] << std::endl;
+        std::cout << myArray[22] << std::endl;
     }
-    for (unsigned int i = 0; i < myArray.size(); i++)
-        std::cout << myArray[i] << std::endl;
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 }
 
 int main(void)
 {
     execute_array();
-    while (1);
+    // while (1);
     return 0;
 }
